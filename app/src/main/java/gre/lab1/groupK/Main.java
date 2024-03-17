92070aaef4e99a38695d60e508fbaf7a96d2cd01
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-03-17 12:37:29 +0100

    Fix file not found and base folder in gradle

--- app/src/main/java/gre/lab1/groupK/Main.java
+++ app/src/main/java/gre/lab1/groupK/Main.java
@@ -2,19 +2,26 @@ package gre.lab1.groupK;
 
 import java.io.IOException;
 
+import gre.lab1.graph.DirectedGraph;
+import gre.lab1.graph.DirectedGraphReader;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("hey");
+        System.out.println("Labo 1 - GRE");
         // Lecture d'un graphe orienté depuis un fichier
-        // DirectedGraph graph = DirectedGraphReader.fromFile("data/file.txt");
+        DirectedGraph graph = DirectedGraphReader.fromFile("data/chaine1.txt");
+
+        // Apply the Tarjan algorithm
+        TarjanAlgorithm ta = new TarjanAlgorithm();
+        System.out.println(ta.compute(graph));
 
         // TODO
-        // - Renommage du package ;
         // - Écrire le code dans les fichiers Main.java, TarjanAlgorithm.java,
         // ContractionAlgorithm.java
         // et UNIQUEMENT ceux-ci ;
         // - Documentation soignée comprenant :
         // - la javadoc, avec auteurs et description des implémentations ;
         // - des commentaires sur les différentes parties de vos algorithmes.
+
     }
 }
