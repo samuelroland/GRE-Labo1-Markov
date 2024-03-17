d9e587b0c0e4736bb9768a30514519136392b78a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-03-17 13:38:10 +0100

    Adapt Main to read both files

--- app/src/main/java/gre/lab1/groupK/Main.java
+++ app/src/main/java/gre/lab1/groupK/Main.java
@@ -8,13 +8,17 @@ import gre.lab1.graph.DirectedGraphReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Labo 1 - GRE");
-        // Lecture d'un graphe orienté depuis un fichier
-        DirectedGraph graph = DirectedGraphReader.fromFile("data/chaine1.txt");
+        final String[] FILES = new String[] { "data/chaine1.txt", "data/chaine2.txt" };
+        for (var file : FILES) {
+            System.out.println("\n\nCalculating the components of the graph in " + file + ":");
+            // Lecture d'un graphe orienté depuis un fichier
+            DirectedGraph graph = DirectedGraphReader.fromFile(file);
 
-        // Apply the Tarjan algorithm
-        TarjanAlgorithm ta = new TarjanAlgorithm();
-        System.out.println(ta.compute(graph));
+            // Apply the Tarjan algorithm
+            TarjanAlgorithm ta = new TarjanAlgorithm();
+            System.out.println(ta.compute(graph));
 
+        }
         // TODO
         // - Écrire le code dans les fichiers Main.java, TarjanAlgorithm.java,
         // ContractionAlgorithm.java
