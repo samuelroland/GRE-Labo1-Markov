005d5184d9836c56593dbdb2251adaffd38e5f27
Jmuhleman <julien.muhlemann@heig-vd.ch>
2024-03-22 13:07:34 +0100

    add contraction

--- app/src/main/java/gre/lab1/groupK/ContractionAlgorithm.java
+++ app/src/main/java/gre/lab1/groupK/ContractionAlgorithm.java
@@ -16,21 +16,22 @@ public class ContractionAlgorithm implements GenericAlgorithm<GraphCondensation>
 
         TarjanAlgorithm tarjan = new TarjanAlgorithm();
         GraphScc graphScc = tarjan.compute(graph);
-        int[] stronglyConnectedComponents = graphScc.components();
+        int[] scc = graphScc.components();
 
         DirectedGraph condensationGraph = new DirectedGraph(graphScc.count());
 
+        int[] list = new int[graphScc.count()];
         // Parcours du graphe d'origine pour ajouter les arêtes dans le graphe condensé
-        for (int vertex = 0; vertex < stronglyConnectedComponents.length; vertex++) {
+        for (int vertex = 0; vertex < scc.length; vertex++) {
             for (int successor : graph.getSuccessorList(vertex)) {
                 //TODO demander l'avis du prof pour le numéro des indexes. # des SCC commence à 1 alors que les indexes des sommets commencent à 0
                 //TODO demander pour passer un GraphScc en paramètre -> avoir accès à la liste des SCC ici. pas possible avec DirectedGraph.
                 //si pour chaque paire de sommet dans le graphe d'origine, u et v appartiennent à des composantes fortement connexes différentes,
                 // alors on ajoute une arête entre les composantes fortement connexes de u et v dans le graphe condensé
-                if (stronglyConnectedComponents[vertex] != stronglyConnectedComponents[successor]) {
-                    condensationGraph.addEdge(stronglyConnectedComponents[vertex] - 1, stronglyConnectedComponents[successor] - 1);
+                if (scc[vertex] != scc[successor] && list[scc[successor]] != scc[vertex]) {
+                    condensationGraph.addEdge(scc[vertex] - 1, scc[successor] - 1);
+                    list[scc[successor]] = scc[vertex];
                 }
-
             }
         }
 
@@ -39,8 +40,8 @@ public class ContractionAlgorithm implements GenericAlgorithm<GraphCondensation>
         for (int i = 0; i < graphScc.count(); i++) {
             mapping.add(new ArrayList<>());
         }
-        for (int vertex = 0; vertex < stronglyConnectedComponents.length; vertex++) {
-            mapping.get(stronglyConnectedComponents[vertex] - 1).add(vertex);
+        for (int vertex = 0; vertex < scc.length; vertex++) {
+            mapping.get(scc[vertex] - 1).add(vertex);
         }
         return new GraphCondensation(graph, condensationGraph, mapping);
 
