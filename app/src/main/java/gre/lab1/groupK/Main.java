5c431717999e2ebe04927b9031c452075a2103c2
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-03-22 15:10:04 +0100

    Implement display of final scc stats

--- app/src/main/java/gre/lab1/groupK/Main.java
+++ app/src/main/java/gre/lab1/groupK/Main.java
@@ -1,6 +1,8 @@
 package gre.lab1.groupK;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import gre.lab1.graph.*;
 
@@ -9,7 +11,7 @@ public class Main {
         System.out.println("Labo 1 - GRE");
         final String[] FILES = new String[] { "data/chaine1.txt", "data/chaine2.txt" };
         for (var file : FILES) {
-            System.out.println("\n\nCalculating the components of the graph in " + file + ":");
+            System.out.println("\n\n>> Calculating the SCC of the graph in " + file + ":");
             // Lecture d'un graphe orienté depuis un fichier
             DirectedGraph graph = DirectedGraphReader.fromFile(file);
 
@@ -17,7 +19,17 @@ public class Main {
             ContractionAlgorithm ca = new ContractionAlgorithm(new TarjanAlgorithm());
             GraphCondensation graphCond = ca.compute(graph);
 
-            
+            // rechercher les états persistants ou transitoires
+            // si degré sortant = 1 et que c'est une boucle alors c'est un état persistant
+            for (var i = 0; i < graphCond.condensation().getNVertices(); i++) {
+                System.out.println("\nStrongly Connected Component: C" + i);
+                boolean isPersistant = graphCond.condensation().getSuccessorList(i).isEmpty();
+                System.out.println("Statut: " + (isPersistant ? "persistant" : "transitive"));
+                System.out.println("Included vertices: " + graphCond.mapping().get(i));
+                if (!isPersistant) {
+                    System.out.println("Successors list: " + graphCond.condensation().getSuccessorList(i));
+                }
+            }
         }
         // TODO
         // - Écrire le code dans les fichiers Main.java, TarjanAlgorithm.java,
