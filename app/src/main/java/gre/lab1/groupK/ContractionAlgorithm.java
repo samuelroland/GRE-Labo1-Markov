68b4a180b347be7378912dcbd25bdb9ccb000d2f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-03-22 14:17:00 +0100

    Make ContractionAlgo indirectly call tarjan algo via dependency injection

--- app/src/main/java/gre/lab1/groupK/ContractionAlgorithm.java
+++ app/src/main/java/gre/lab1/groupK/ContractionAlgorithm.java
@@ -4,18 +4,22 @@ import gre.lab1.graph.DirectedGraph;
 import gre.lab1.graph.GenericAlgorithm;
 import gre.lab1.graph.GraphCondensation;
 import gre.lab1.graph.GraphScc;
+import gre.lab1.graph.SccAlgorithm;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContractionAlgorithm implements GenericAlgorithm<GraphCondensation> {
 
+    private SccAlgorithm sccAlgo;
+
+    public ContractionAlgorithm(SccAlgorithm sccAlgo) {
+        this.sccAlgo = sccAlgo;
+    }
 
     @Override
     public GraphCondensation compute(DirectedGraph graph) {
-
-        TarjanAlgorithm tarjan = new TarjanAlgorithm();
-        GraphScc graphScc = tarjan.compute(graph);
+        GraphScc graphScc = sccAlgo.compute(graph);
         int[] scc = graphScc.components();
 
         DirectedGraph condensationGraph = new DirectedGraph(graphScc.count());
@@ -24,9 +28,7 @@ public class ContractionAlgorithm implements GenericAlgorithm<GraphCondensation>
         // Parcours du graphe d'origine pour ajouter les arêtes dans le graphe condensé
         for (int vertex = 0; vertex < scc.length; vertex++) {
             for (int successor : graph.getSuccessorList(vertex)) {
-                //TODO demander l'avis du prof pour le numéro des indexes. # des SCC commence à 1 alors que les indexes des sommets commencent à 0
-                //TODO demander pour passer un GraphScc en paramètre -> avoir accès à la liste des SCC ici. pas possible avec DirectedGraph.
-                //si pour chaque paire de sommet dans le graphe d'origine, u et v appartiennent à des composantes fortement connexes différentes,
+                // si pour chaque paire de sommet dans le graphe d'origine, u et v appartiennent à des composantes fortement connexes différentes,
                 // alors on ajoute une arête entre les composantes fortement connexes de u et v dans le graphe condensé
                 if (scc[vertex] != scc[successor] && list[scc[successor]] != scc[vertex]) {
                     condensationGraph.addEdge(scc[vertex] - 1, scc[successor] - 1);
--- app/src/main/java/gre/lab1/groupK/Main.java
+++ app/src/main/java/gre/lab1/groupK/Main.java
@@ -2,8 +2,7 @@ package gre.lab1.groupK;
 
 import java.io.IOException;
 
-import gre.lab1.graph.DirectedGraph;
-import gre.lab1.graph.DirectedGraphReader;
+import gre.lab1.graph.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,10 +13,11 @@ public class Main {
             // Lecture d'un graphe orienté depuis un fichier
             DirectedGraph graph = DirectedGraphReader.fromFile(file);
 
-            // Apply the Tarjan algorithm
-            TarjanAlgorithm ta = new TarjanAlgorithm();
-            System.out.println(ta.compute(graph));
+            // Apply the GraphCondensation algorigthm using the Tarjan algorithm
+            ContractionAlgorithm ca = new ContractionAlgorithm(new TarjanAlgorithm());
+            GraphCondensation graphCond = ca.compute(graph);
 
+            
         }
         // TODO
         // - Écrire le code dans les fichiers Main.java, TarjanAlgorithm.java,
