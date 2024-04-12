df0f008851d27ec3d2659fc2fa92eb4ccefb7696
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-04-12 13:37:29 +0200

    Translate Main

--- app/src/main/java/gre/lab1/groupK/Main.java
+++ app/src/main/java/gre/lab1/groupK/Main.java
@@ -1,8 +1,6 @@
 package gre.lab1.groupK;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import gre.lab1.graph.*;
 
@@ -12,6 +10,7 @@ import gre.lab1.graph.*;
 public class Main {
     /**
      * Takes some files, each one representing a graph, print their SCC with their characteristics.
+     * 
      * @param args No arg is needed
      * @throws IOException
      */
@@ -20,15 +19,15 @@ public class Main {
         final String[] FILES = new String[] { "data/chaine1.txt", "data/chaine2.txt" };
         for (var file : FILES) {
             System.out.println("\n\n>> Calculating the SCC of the graph in " + file + ":");
-            // Lecture d'un graphe orienté depuis un fichier
+            // Read the given graph file, parse it and return a DirectedGraph structure
             DirectedGraph graph = DirectedGraphReader.fromFile(file);
 
             // Apply the GraphCondensation algorigthm using the Tarjan algorithm
             ContractionAlgorithm ca = new ContractionAlgorithm(new TarjanAlgorithm());
             GraphCondensation graphCond = ca.compute(graph);
 
-            // rechercher les états persistants ou transitoires
-            // si degré sortant = 1 et que c'est une boucle alors c'est un état persistant
+            // Search for persistant or transitive states of each vertex of the condensated graph
+            // and show the scc number, the state, the included vertices and the successors list if persistant
             for (var i = 0; i < graphCond.condensation().getNVertices(); i++) {
                 System.out.println("\nStrongly Connected Component: C" + i);
                 boolean isPersistant = graphCond.condensation().getSuccessorList(i).isEmpty();
@@ -39,9 +38,6 @@ public class Main {
                 }
             }
         }
-        // TODO
-        // - Écrire le code dans les fichiers Main.java, TarjanAlgorithm.java,
-        // ContractionAlgorithm.java
         // et UNIQUEMENT ceux-ci ;
         // - Documentation soignée comprenant :
         // - la javadoc, avec auteurs et description des implémentations ;
